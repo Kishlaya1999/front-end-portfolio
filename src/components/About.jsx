4281347a798e015f684fd7b1d36861732b8ffaa6
Kishlaya1999 <kishlaya54321@gmail.com>
2023-03-07 23:51:14 +0530

    Basic About done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,24 @@ import React from "react";
 const About = () => {
 	return (
 		<div id="about" className="about">
-			<h1 data-aos="fade-right">About</h1>
+			<div className="about-container">
+				<span className="about-heading" data-aos="fade-right">
+					About
+				</span>
+				<p data-aos="fade-left">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque
+					magnam odio repellendus, dolore debitis voluptatem sapiente corrupti ipsum
+					sequi alias tenetur quod mollitia neque delectus, iure pariatur. Sequi
+					itaque animi eveniet, rem ullam quo ipsa maiores iure, rerum recusandae
+					reprehenderit nesciunt hic assumenda deserunt dolores. Reprehenderit
+					dolorem est quibusdam ea delectus corporis distinctio minus nulla id,
+					temporibus itaque optio impedit, illum cum. Tenetur, pariatur sapiente.
+					Repudiandae consequatur esse doloremque iste eveniet! Repudiandae
+					aspernatur ducimus doloremque saepe ea est enim eos. Ipsa cupiditate
+					consectetur, ducimus quo error debitis id maxime deleniti magni blanditiis
+					tempora aliquam fuga quod odit iusto laborum?
+				</p>
+			</div>
 		</div>
 	);
 };
