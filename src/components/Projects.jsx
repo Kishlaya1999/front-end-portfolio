1d00ab792a437426ce57135ae6697272c6dd9bc4
Kishlaya1999 <kishlaya54321@gmail.com>
2023-03-11 23:18:55 +0530

    imagehover.css library installed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import HTML from "../assets/skill-section-img/skills/HTML.png";
 
 const Projects = () => {
 	return (
@@ -8,7 +9,24 @@ const Projects = () => {
 			<div className="project-container">
 				<span className="projects-heading">Projects</span>
 				<div className="personal-projects">
-					<div className="project"></div>
+					<div className="project">
+						<figure class="imghvr-stack-up">
+							<img src={HTML} alt="example-image" />
+							<figcaption>
+								<h3 class="ih-fade-down ih-delay-sm ">Oscar Wilde</h3>
+								<p class="ih-zoom-in ih-delay-md">
+									<i>
+										"I have the simplest tastes, I am always satisfied
+										with the best."
+									</i>
+								</p>
+								<a class="ih-fade-up ih-delay-lg button" href="#">
+									Read More
+								</a>
+							</figcaption>
+							<a href="#"></a>
+						</figure>
+					</div>
 					<div className="project"></div>
 					<div className="project"></div>
 				</div>
