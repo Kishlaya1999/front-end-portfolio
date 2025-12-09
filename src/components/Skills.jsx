7d48bb08ac4c88f7c689518fbe2d0355ced6d07b
Kishlaya1999 <kishlayacs9935996006@gmail.com>
2025-12-10 01:54:06 +0530

    feat: Enhance skill display with titles and responsive styling, and improve header layout responsiveness.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -48,14 +48,14 @@ const Skills = () => {
         <div className="skill-category">
           <h3>Frontend</h3>
           <div className="skill-grid">
-            <div className="skill-item">
-              <Icon icon="logos:javascript" /> JavaScript
+            <div className="skill-item" title="JavaScript">
+              <Icon icon="logos:javascript" /> <span className="skill-name">JavaScript</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="logos:react" /> React
+            <div className="skill-item" title="React">
+              <Icon icon="logos:react" /> <span className="skill-name">React</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="logos:redux" /> Redux
+            <div className="skill-item" title="Redux">
+              <Icon icon="logos:redux" /> <span className="skill-name">Redux</span>
             </div>
             {/* <div className="skill-item">
               <Icon icon="vscode-icons:file-type-reactrouter" /> React Router
@@ -66,23 +66,23 @@ const Skills = () => {
             <div className="skill-item">
               <Icon icon="simple-icons:mui" color="#0472E7" /> Material UI
             </div> */}
-            <div className="skill-item">
-              <Icon icon="logos:sass" /> Sass
+            <div className="skill-item" title="Sass">
+              <Icon icon="logos:sass" /> <span className="skill-name">Sass</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="logos:tailwindcss-icon" /> Tailwind CSS
+            <div className="skill-item" title="Tailwind CSS">
+              <Icon icon="logos:tailwindcss-icon" /> <span className="skill-name">Tailwind CSS</span>
             </div>
             {/* <div className="skill-item">
               <Icon icon="devicon:reactbootstrap" /> React Bootstrap
             </div> */}
-            <div className="skill-item">
-              <Icon icon="logos:bootstrap" /> Bootstrap
+            <div className="skill-item" title="Bootstrap">
+              <Icon icon="logos:bootstrap" /> <span className="skill-name">Bootstrap</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="vscode-icons:file-type-html" /> HTML
+            <div className="skill-item" title="HTML">
+              <Icon icon="vscode-icons:file-type-html" /> <span className="skill-name">HTML</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="devicon:css3" /> CSS
+            <div className="skill-item" title="CSS">
+              <Icon icon="devicon:css3" /> <span className="skill-name">CSS</span>
             </div>
           </div>
         </div>
@@ -91,17 +91,17 @@ const Skills = () => {
         <div className="skill-category">
           <h3>Backend</h3>
           <div className="skill-grid">
-            <div className="skill-item">
-              <Icon icon="vscode-icons:file-type-node" /> Node.js
+            <div className="skill-item" title="Node.js">
+              <Icon icon="vscode-icons:file-type-node" />  <span className="skill-name">Node.js</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="simple-icons:express" color="#fff" /> Express
+            <div className="skill-item" title="Express">
+              <Icon icon="simple-icons:express" color="#fff" /> <span className="skill-name">Express</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="devicon:mongodb" /> MongoDB
+            <div className="skill-item" title="MongoDB">
+              <Icon icon="devicon:mongodb" />   <span className="skill-name">MongoDB</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="logos:jwt-icon" /> JWT
+            <div className="skill-item" title="JWT">
+              <Icon icon="logos:jwt-icon" /> <span className="skill-name">JWT</span>
             </div>
             {/* <div className="skill-item">
               <Icon icon="simple-icons:socketdotio" color="#fff" /> Socket.io
@@ -113,17 +113,17 @@ const Skills = () => {
         <div className="skill-category">
           <h3>Tools</h3>
           <div className="skill-grid">
-            <div className="skill-item">
-              <Icon icon="logos:git-icon" /> Git
+            <div className="skill-item" title="Git">
+              <Icon icon="logos:git-icon" /> <span className="skill-name">Git</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="mdi:github" color="#fff" /> GitHub
+            <div className="skill-item" title="GitHub">
+              <Icon icon="mdi:github" color="#fff" /> <span className="skill-name">GitHub</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="devicon:jira" /> Jira
+            <div className="skill-item" title="Jira">
+              <Icon icon="devicon:jira" /> <span className="skill-name">Jira</span>
             </div>
-            <div className="skill-item">
-              <Icon icon="logos:postman-icon" /> Postman
+            <div className="skill-item" title="Postman">
+              <Icon icon="logos:postman-icon" /> <span className="skill-name">Postman</span>
             </div>
             {/* <div className="skill-item">
               <Icon icon="logos:new-relic-icon" /> New Relic
